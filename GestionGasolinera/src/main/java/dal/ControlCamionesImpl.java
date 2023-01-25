914888c86f382e8dee1e71e77e3f532c029da090
DESKTOP-3I3N22C\JesusPatricio <csi22-jpatano@colegioaltair.net>
2023-01-25 14:38:51 +0100

    Delete

--- GestionGasolinera/src/main/java/dal/ControlCamionesImpl.java
+++ GestionGasolinera/src/main/java/dal/ControlCamionesImpl.java
@@ -1,5 +1,6 @@
 package dal;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.EntityManager;
@@ -17,6 +18,12 @@ public class ControlCamionesImpl implements ControlCamionesServicio{
 		emCamiones.persist(controlCamiones);
 		emCamiones.clear();
 		emCamiones.close();
+	}
+	
+	public void eliminarCombustible(ControlCamiones controlCamiones){
+		List<ControlCamiones> lista= new ArrayList<ControlCamiones>();
+		lista=emCamiones.createQuery("SELECT combustible FROM ControlCamiones combustible").getResultList();
+		int ultimoReg=lista.size();
 	}
 
 	public List<ControlCamiones> buscarCamiones() {
